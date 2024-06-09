a5de8709ebde083a4576f99b8f6c04de1221e16b
6dean <ferdjallahrockdean@gmail.com>
2024-06-09 23:06:05 +0200

    footer correction

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="bottom-0 w-full p-4 text-white">
+    <div className="bottom-0 w-full p-4 text-white mt-10">
       <div className="flex justify-center">
         <span className="textfooter">Made by RF - Powered by Next.js</span>
       </div>
